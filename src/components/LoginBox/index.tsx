4a7676e2d01d025b5429f9981efbe6cb8d630850
gabyrodrigues <gabyrlima.99@gmail.com>
2022-04-07 21:29:52 -0300

    creating auth context api

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,44 +1,10 @@
 import styles from './styles.module.scss'
 import { VscGithubInverted } from 'react-icons/vsc'
-import { useEffect } from 'react'
-import { api } from '../../services/api'
-
-type AuthResponse = { 
-  token: string;
-  user: {
-    id: string;
-    avatar_url: string;
-    name: string;
-    login: string;
-  }
-}
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/auth'
 
 export function LoginBox() {
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=5b5706ac72655d62d0de`
-
-  async function signIn(githubCode: string) {
-    const response = await api.post<AuthResponse>('/authenticate', {
-      code: githubCode
-    })
-
-    const { token, user } = response.data
-
-    localStorage.setItem('@dowhile:token', token)
-    console.log(user)
-  }
-
-  useEffect(() => {
-    const url = window.location.href
-    const hasGithubCode = url.includes('?code=')
-
-    if(hasGithubCode) {
-      const [urlWithoutCode, githubCode] = url.split('?code=') //splits url with and without code
-      
-      window.history.pushState({}, '', urlWithoutCode) //shows new url without code after authorization
-
-      signIn(githubCode)
-    }
-  }, [])
+  const { signInUrl, user } = useContext(AuthContext)
 
   return (
     <div className={styles.loginBoxWrapper}>
---
+++ src/contexts/auth.tsx
@@ -0,0 +1,83 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+}
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+  signOut: () => void;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+type AuthProvider = {
+  children: ReactNode;
+}
+
+type AuthResponse = { 
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
+
+export function AuthProvider(props: AuthProvider) {
+  const [user, setUser] = useState<User | null>(null)
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=5b5706ac72655d62d0de`
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('/authenticate', {
+      code: githubCode
+    })
+
+    const { token, user } = response.data
+
+    localStorage.setItem('@dowhile:token', token)
+    setUser(user)
+  }
+
+  function signOut() {
+    setUser(null)
+    localStorage.removeItem('@dowhile:token')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@dowhile:token')
+
+    if(token) {
+      api.defaults.headers.common.authorization =  `Bearer ${token}`
+
+      api.get<User>('/profile').then(response => {
+        setUser(response.data)
+      })
+    }
+  })
+
+  useEffect(() => {
+    const url = window.location.href
+    const hasGithubCode = url.includes('?code=')
+
+    if(hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=') //splits url with and without code
+      
+      window.history.pushState({}, '', urlWithoutCode) //shows new url without code after authorization
+
+      signIn(githubCode)
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ signInUrl, user, signOut }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
