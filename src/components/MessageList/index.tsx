09e45ca5d117fc183c490da94a2e1db18d04fa95
gabyrodrigues <gabyrlima.99@gmail.com>
2022-04-07 23:38:02 -0300

    creating new message socket and listing last 3 messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -4,6 +4,7 @@ import { api } from '../../services/api'
 import styles from './styles.module.scss'
 
 import logoImg from '../../assets/logo.svg'
+import { io } from 'socket.io-client'
 
 type Message = {
   id: string;
@@ -14,8 +15,30 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = []
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage) //insert sent messages on queue
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    setInterval(() => {
+      if(messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0], //in array first position, insert first (oldest) message from queue
+          prevState[0], //get already existing messages from first position of messages array
+          prevState[1] //get already existing messages from second position of messages array
+        ].filter(Boolean)) //filter to remove undefined/null values
+
+        messagesQueue.shift()
+      }
+    }, 3000)
+  }, [])
 
   useEffect(() => {
     api.get<Message[]>('/messages/last3').then(response => {
