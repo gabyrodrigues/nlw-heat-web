ffda1c583c3f355fda39d264ec8002c415153c6c
gabyrodrigues <gabyrlima.99@gmail.com>
2022-04-13 23:03:17 -0300

    fix timer messages list interval

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -27,7 +27,7 @@ export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([])
 
   useEffect(() => {
-    setInterval(() => {
+    const timer = setInterval(() => {
       if(messagesQueue.length > 0) {
         setMessages(prevState => [
           messagesQueue[0], //in array first position, insert first (oldest) message from queue
@@ -38,6 +38,8 @@ export function MessageList() {
         messagesQueue.shift()
       }
     }, 3000)
+
+    return () => clearInterval(timer)
   }, [])
 
   useEffect(() => {
